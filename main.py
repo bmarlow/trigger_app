2145ce4baa19bc984383a2ed40759f2428ff82e4
Unknown <brandon.n.marlow@gmail.com>
2019-10-12 15:53:38 -0700

    update consumer

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import shutil, kafka, logging, time
 
 def main_loop():
     while True:
-        consumer_received = kafka.KafkaConsumer('file-received', bootstrap_servers='my-cluster-kafka-bootstrap:9092', consumer_timeout_ms=1000)
+        kafka.KafkaConsumer()
+        consumer_received = kafka.KafkaConsumer('file-received', bootstrap_servers='my-cluster-kafka-bootstrap:9092', consumer_timeout_ms=10000)
         for message in consumer_received:
-            print(message.value)
-        time.sleep(1)
+            print(bytes.decode(message.value))
+
 
 if __name__ == "__main__":
     main_loop()
