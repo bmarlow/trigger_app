281204279c3d89dac7932e9e846a0444f33528f3
Unknown <brandon.n.marlow@gmail.com>
2019-10-12 15:54:10 -0700

    removing errant config

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import shutil, kafka, logging, time
 
 def main_loop():
     while True:
-        kafka.KafkaConsumer()
         consumer_received = kafka.KafkaConsumer('file-received', bootstrap_servers='my-cluster-kafka-bootstrap:9092', consumer_timeout_ms=10000)
         for message in consumer_received:
             print(bytes.decode(message.value))
