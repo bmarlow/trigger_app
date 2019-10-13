9c427cb55173d48ce29d8a75a758a69ddb1392ea
Unknown <brandon.n.marlow@gmail.com>
2019-10-13 10:43:03 -0700

    bytes bite

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ def main_loop():
     while True:
         consumer_received = kafka.KafkaConsumer('file-received', bootstrap_servers='my-cluster-kafka-bootstrap:9092', consumer_timeout_ms=10000)
         for message in consumer_received:
+            print(bytes.decode(message.value))
             str_message = bytes.decode(message.value)
-            file = str_message.value.split(': ')[1:]
+            file = str_message.split(': ')[1:]
             files.append(file)
             if files.len() == 2:
                 break
