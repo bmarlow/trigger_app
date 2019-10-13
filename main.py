c8383acdb49fb1ced140eed230ddc31a30f66b61
Unknown <brandon.n.marlow@gmail.com>
2019-10-13 10:34:25 -0700

    stubbing trigger logic

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import urllib.request
 import shutil, kafka, logging, time
 
 def main_loop():
+    count = 0
+    files = []
     while True:
         consumer_received = kafka.KafkaConsumer('file-received', bootstrap_servers='my-cluster-kafka-bootstrap:9092', consumer_timeout_ms=10000)
         for message in consumer_received:
             print(bytes.decode(message.value))
-
+            file = message.value.split(': ')[1:]
+            files.append(file)
+            if files.len() == 2:
+                break
+    #make API call to get files
+    print('you escaped the loop!')
 
 if __name__ == "__main__":
     main_loop()
